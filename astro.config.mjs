bbc7863dbe4219abad760aee5c9832bee7aa815c
Andres Alvarez <andresalvarez353@gmail.com>
2026-06-04 17:11:15 -0600

    Fix Astro locale redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://cheflink.io',
   integrations: [tailwind()],
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: true,
-    },
-  },
 });
