29347a74a252e79418b8a68f97027a1fba67b6b8
Andres Alvarez <andresalvarez353@gmail.com>
2026-08-09 11:38:47 -0600

    Template fixes

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+// Single source of truth for the product name shown across the site.
+export const APP_NAME = 'Cheflink';
+
+// Support contact shown on legal and contact pages.
+export const SUPPORT_EMAIL = 'support@cheflink.io';
